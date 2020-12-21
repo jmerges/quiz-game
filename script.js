0a7b02f850e039276c3bf8d223e6a381314fe05f
jmerges bootcamp <jamesmerges1@gmail.com>
2020-12-21 13:22:25 -0800

    Added nextQuestion() which iterates through the questionArray and displays the new choices

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+var questionEl = document.getElementById("question");
 var aButton = document.getElementById("a");
 var bButton = document.getElementById("b");
 var cButton = document.getElementById("c");
 var dButton = document.getElementById("d");
+var result = document.querySelector("result");
 
 // This array stores the questions and the choices
 var questionArray = [
@@ -11,14 +13,14 @@ var questionArray = [
         "b": "question1b",
         "c": "question1c",
         "d": "question1d",
-    }
+    },
     {
         "question": "Question2",
         "a": "question2a",
         "b": "quesiton2b",
         "c": "question2c",
         "d": "question2d",
-    }
+    },
     {
         "question": "Question3",
         "a": "question3a",
@@ -37,13 +39,23 @@ var correctChoice = "a";
 function userChoice (event) {
     var answer = event.target.id;
     if (answer===correctChoice) {
+        result.textContent = "Correct!";
         nextQuestion();
     }
     else {
+        result.textContent = "Wrong!";
         time-=10;
+        nextQuestion();
     }
 }
 
 function nextQuestion() {
     currentIndex +=1;
+    question.textContent = questionArray[currentIndex]["question"];
+    aButton.textContent = questionArray[currentIndex]["a"];
+    bButton.textContent = questionArray[currentIndex]["b"];
+    cButton.textContent = questionArray[currentIndex]["c"];
+    dButton.textContent = questionArray[currentIndex]["d"];
 }
+
+nextQuestion();
